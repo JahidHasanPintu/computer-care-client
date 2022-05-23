c0d95c23311dcc20ff5516c973e42553205f4ae5
JahidHasanPintu <jahidhasanpintu82@gmail.com>
2022-05-24 00:36:35 +0600

    effective error

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import './App.css';
 import Blog from './Components/Pages/Blog/Blog';
 import Home from './Components/Pages/Home/Home';
 import Login from './Components/Pages/Login/Login';
+import Register from './Components/Pages/Register/Register';
 import Footer from './Components/Pages/Shared/Footer/Footer';
 import Navbar from './Components/Pages/Shared/Navbar/Navbar';
 
@@ -17,6 +18,7 @@ function App() {
         <Route path="/home" element={<Home/>}></Route>
         <Route path="login" element={<Login/>}></Route>
         <Route path="/blog" element={<Blog/>}></Route>
+        <Route path="/register" element={<Register/>}></Route>
       </Routes>
       
       <Footer></Footer>
